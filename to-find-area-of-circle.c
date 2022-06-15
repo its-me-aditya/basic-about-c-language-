c4c3931d667acab550beb5181c445440a0d6e2d3
Aditya singh <107112101+its-me-aditya@users.noreply.github.com>
2022-06-15 16:18:43 +0530

    Create to-find-area-of-circle.c

---
+++ to-find-area-of-circle.c
@@ -0,0 +1,11 @@
+// to find the area of the circle 
+
+#include<stdio.h>
+
+int main(){
+     float radius;
+     scanf("%f",&radius);
+     printf("%f \n",3.14*radius*radius);
+     return 0 ;
+     
+}
